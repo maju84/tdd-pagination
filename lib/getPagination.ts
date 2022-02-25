adb7b0ecc7079c6bfe309c7d2653bceb5a178e09
maju84 <max-j@gmx.de>
2022-02-25 18:49:24 +0100

    getPagination returns '1 2 (3) 4' for page 3 of 4.

--- lib/getPagination.ts
+++ lib/getPagination.ts
@@ -2,7 +2,21 @@ const getPagination = ({ currentPage, totalPages }: {
   currentPage: number;
   totalPages: number;
 }): string => {
-  return '(1)';
+  if (currentPage === 1 && totalPages === 1) {
+    return '(1)';
+  }
+
+  const pages = [];
+
+  for (let page =1; page<=totalPages; page++){
+    if (page === currentPage) {
+      pages.push(`(${page})`);
+    } else {
+      pages.push(page);
+    }    
+  }
+
+  return pages.join(' ');
 };
 
 export { getPagination };
--- test/unit/getPagination.Tests.ts
+++ test/unit/getPagination.Tests.ts
@@ -15,4 +15,17 @@ suite('getPagination', (): void => {
       // Assert
       assert.that(pagination).is.equalTo('(1)');
     });
+
+   test(`returns '1 2 (3) 4' for page 3 of 4.`,
+    async (): Promise<void> => { 
+      // Arrange
+      const currentPage = 3,
+            totalPages = 4;
+
+      // Act
+      const pagination = getPagination({ currentPage, totalPages });
+
+      // Assert
+      assert.that(pagination).is.equalTo('1 2 (3) 4');
+    });
 });
