80a57c5eeb8404d70c6e4cb05d847c7949bae045
maju84 <max-j@gmx.de>
2022-02-26 18:42:19 +0100

    refactor

--- lib/getPagination.ts
+++ lib/getPagination.ts
@@ -1,3 +1,21 @@
+const getPaginationForRange = ({ from, to, current }: {
+  from: number;
+  to: number;
+  current: number;
+}): string[] => {
+  const pages = [];
+
+  for (let page = from; page <= to; page++) {
+    if (page === current) {
+      pages.push(`(${page})`);
+    } else {
+      pages.push(`${page}`);
+    }
+  }
+
+  return pages;
+};
+
 const getPagination = ({ currentPage, totalPages }: {
   currentPage: number;
   totalPages: number;
@@ -12,54 +30,27 @@ const getPagination = ({ currentPage, totalPages }: {
     throw new Error('currentPage must be lower or equal to totalPages.');
   }
 
+  if (totalPages <= 7) {
+    return getPaginationForRange({ from: 1, to: totalPages, current: currentPage }).join(' ');
+  }
+
   const pages = [];
 
-  if (totalPages > 7) {
+  if (currentPage < 5) {
+    pages.push(...getPaginationForRange({ from: 1, to: 5, current: currentPage }));
+    pages.push(`... ${totalPages}`);
 
-    if (currentPage < 5){
-    
-      for (let page = 1; page <= 5; page++) {
-        if (page === currentPage) {
-          pages.push(`(${page})`);
-        } else {
-          pages.push(page);
-        }
-      }
-
-      pages.push(`... ${totalPages}`);
-      return pages.join(' ');
-
-    }
-
-    if (currentPage > totalPages-4 ){
-    
-      pages.push(`1 ...`); 
-      for (let page = totalPages-4; page <= totalPages; page++) {
-        if (page === currentPage) {
-          pages.push(`(${page})`);
-        } else {
-          pages.push(page);
-        }
-      }
-      
-      return pages.join(' ');
-
-    }
-
-    return `1 ... ${currentPage-1} (${currentPage}) ${currentPage+1} ... ${totalPages}`;
-
+    return pages.join(' ');
   }
-  
-  for (let page = 1; page <= totalPages; page++) {
-    if (page === currentPage) {
-      pages.push(`(${page})`);
-    } else {
-      pages.push(page);
-    }
-  }
-    
 
-  return pages.join(' ');
+  if (currentPage > totalPages - 4) {
+    pages.push(`1 ...`);
+    pages.push(...getPaginationForRange({ from: totalPages - 4, to: totalPages, current: currentPage }));
+
+    return pages.join(' ');
+  }
+
+  return `1 ... ${currentPage - 1} (${currentPage}) ${currentPage + 1} ... ${totalPages}`;
 };
 
 export { getPagination };
