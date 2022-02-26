a740f9d08c64c1f9e38c56059f1605317e8956d7
maju84 <max-j@gmx.de>
2022-02-26 15:29:27 +0100

    add test data for 3 additional test cases

--- test/unit/getPagination.Tests.ts
+++ test/unit/getPagination.Tests.ts
@@ -4,7 +4,10 @@ import { getPagination } from '../../lib/getPagination';
 suite('getPagination', (): void => {
   const values = [
     { current: 1, total: 1, expected: '(1)' },
-    { current: 3, total: 4, expected: '1 2 (3) 4' }
+    { current: 3, total: 4, expected: '1 2 (3) 4' },
+    { current: 1, total: 7, expected: '(1) 2 3 4 5 6 7' },
+    { current: 3, total: 7, expected: '1 2 (3) 4 5 6 7' },
+    { current: 7, total: 7, expected: '1 2 3 4 5 6 (7)' }
   ];
 
   for (const { current, total, expected } of values) {
